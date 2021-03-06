7971bb38fe7f4dddb89dcd979513c66dd0100a42
KyleLee15 <45340450+KyleLee15@users.noreply.github.com>
2021-03-06 23:41:38 +0000

    Add files via upload

--- NCCPack/config.lua
+++ NCCPack/config.lua
@@ -8,27 +8,19 @@
                  Made By KyleLee15 (https://github.com/KyleLee15/NCCPack)
 ]]
 
--- Define the FX Server version and game type
-fx_version "adamant"
-game "gta5"
 
--- Manifest
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+Config = {}
 
--- Resource Info
-name 'KyleLee15 | NCCPack'
-description 'FiveM Standalone Chat With Changeable Name & Department'
-author 'KyleLee15 (https://github.com/KyleLee15)'
-version '1.0.0'
-url 'https://github.com/KyleLee15/NCCPack'
-
--- Script
-shared_script 'config.lua'
-
-client_scripts {
-	'chat/ncc-c.lua'
-}
-server_script {
-	'chat/ncc-s.lua',
-	'webhook/webhook_s.lua'
-}
+EnableChatSuggestions = true -- ENABLE/DISABLE Chat Suggestions
+EnableChatCommand = true -- If True The Chat Commands Will Be Enabled
+    EnableStandChat = true -- This is to ENABLE/DISABLE the chat input when you do not enter a command 
+    EnableMeChat = true -- This is to ENABLE/DISABLE the /me command
+    EnableMerChat = true -- This is to ENABLE/DISABLE the /mer command
+    EnableOOCChat = true -- This is to ENABLE/DISABLE the /ooc command
+    EnableOOCRChat = true -- This is to ENABLE/DISABLE the /oocr command
+    EnableGOOCChat = true -- This is to ENABLE/DISABLE the /gooc command
+    EnableDwChat = true -- This is to ENABLE/DISABLE the /dw command
+    
+EnableNameChange = true -- Enable /name feture
+EnableDeptChange = true -- Enable /department feture
+EnableTwtCommands = true -- Enable the Twotter Fetures (/twt & /twthandle)
