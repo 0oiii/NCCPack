19848d49741b5af539e16d06a660e84cd34fac5f
KyleLee15 <45340450+KyleLee15@users.noreply.github.com>
2021-03-21 21:37:19 +0000

    Update fxmanifest.lua

--- NCCPack/fxmanifest.lua
+++ NCCPack/fxmanifest.lua
@@ -19,7 +19,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 name 'KyleLee15 | NCCPack'
 description 'FiveM Standalone Chat With Changeable Name & Department'
 author 'KyleLee15 (https://github.com/KyleLee15)'
-version '1.0.0'
+version '1.0.1'
 url 'https://github.com/KyleLee15/NCCPack'
 
 -- Script
